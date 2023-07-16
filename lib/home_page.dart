addf431b689f6994a32f161cb5d4178a068f579f
Chandra Aditya Rahman <chandrarahman667@gmail.com>
2023-07-16 15:57:52 +0700

    new file:   assets/biji.png 	new file:   assets/coffee_1.jpg 	new file:   assets/coffee_2.jpg 	new file:   assets/coffee_3.jpg 	new file:   assets/cup.png 	new file:   assets/mesin.png 	new file:   assets/tubruk.png 	modified:   lib/home_page.dart 	new file:   lib/widget/category.dart 	new file:   lib/widget/coffee_shop.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:coffee_shop/widget/category.dart';
+import 'package:coffee_shop/widget/coffee_shop.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,76 +9,147 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark), label: "Tersimpan"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
-        ]),
-        body: SafeArea(
-            child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: 140,
-                  width: double.infinity,
-                  color: Colors.brown,
-                ),
-                Column(
-                  children: [
-                    SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                alignment: Alignment.topLeft,
-                                height: 45,
-                                width: 45,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: NetworkImage(
-                                      'https://oliver-andersen.se/wp-content/uploads/2018/03/cropped-Profile-Picture-Round-Color.png',
-                                    )),
-                                    borderRadius: BorderRadius.circular(25),
-                                    border: Border.all(
-                                      color: Colors.white,
-                                      style: BorderStyle.solid,
-                                      width: 2,
-                                    )),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                'Halo Chan, Selamat Datang !',
-                                style: GoogleFonts.poppins(
-                                  color: Colors.white,
-                                  fontSize: 14,
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: "Tersimpan"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+      ]),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 140,
+                    width: double.infinity,
+                    color: Colors.brown,
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.topLeft,
+                                  height: 45,
+                                  width: 45,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                        'https://oliver-andersen.se/wp-content/uploads/2018/03/cropped-Profile-Picture-Round-Color.png',
+                                      )),
+                                      borderRadius: BorderRadius.circular(25),
+                                      border: Border.all(
+                                        color: Colors.white,
+                                        style: BorderStyle.solid,
+                                        width: 2,
+                                      )),
                                 ),
-                              ),
-                            ],
-                          ),
-                          Container(
-                            alignment: Alignment.topRight,
-                            child: Icon(
-                              Icons.notifications_active,
-                              color: Colors.white,
-                              size: 30,
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Halo Chan, Selamat Datang !',
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
                             ),
-                          )
-                        ],
+                            Container(
+                              alignment: Alignment.topRight,
+                              child: Icon(
+                                Icons.notifications_active,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    )
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Container(
+                          height: 60,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Color(0xFFF5F5F7),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: TextField(
+                            cursorHeight: 20,
+                            autofocus: false,
+                            decoration: InputDecoration(
+                              hintText: "Cari Toko Kopi Favoritmu!",
+                              prefixIcon: Icon(Icons.search),
+                              border: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 2),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  children: [
+                    Category(imagePath: "assets/tubruk.png", title: "Tubruk"),
+                    Category(imagePath: "assets/mesin.png", title: "Espresso"),
+                    Category(imagePath: "assets/cup.png", title: "Latte"),
+                    Category(imagePath: "assets/biji.png", title: "Biji"),
                   ],
                 ),
-              ],
-            )
-          ],
-        )));
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  "Tempat Favorit☕️",
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              CoffeShop(
+                imagePath: "assets/coffee_1.jpg",
+                nameShop: "Kopi Kenangan",
+                rating: "4.8",
+                openTime: "10.00 - 23.00",
+              ),
+              CoffeShop(
+                imagePath: "assets/coffee_2.jpg",
+                nameShop: "Kopi Janji Jiwa",
+                rating: "4.6",
+                openTime: "09.00 - 22.00",
+              ),
+              CoffeShop(
+                imagePath: "assets/coffee_3.jpg",
+                nameShop: "Kopi Tuku",
+                rating: "4.5",
+                openTime: "09.30.00 - 21.00",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
